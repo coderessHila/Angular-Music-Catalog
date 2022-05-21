75d20db14623d39ac5d8d82c9876573336fb9090
Hila Sackstein <hila.sackstein@Hilas-MacBook-Pro.local>
2022-05-21 19:40:37 +0300

    preview img size

--- src/app/artists-data/artists.mock.ts
+++ src/app/artists-data/artists.mock.ts
@@ -30,6 +30,34 @@ export const ARTISTS: IArtist[] = [
       country: 'United Kingdom',
       city: 'Aston, Birmingham'
     }
+  },
+  {
+    id: '4',
+    name: 'Fleetwood Mac',
+    imgUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Fleetwood_Mac_Billboard_1977.jpg/640px-Fleetwood_Mac_Billboard_1977.jpg',
+    genres: ['pop rock', 'folk rock', 'blues rock'],
+    origin: {
+      country: 'United Kingdom',
+      city: 'London'
+    }
+  },{
+    id: '5',
+    name: 'The Supremes',
+    imgUrl: 'https://www.udiscovermusic.com/wp-content/uploads/2020/02/The-Supremes-GettyImages-74001231.jpg',
+    genres: ['Motown', 'Blues'],
+    origin: {
+      country: 'US',
+      city: 'Detroit, Michigan'
+    }
+  },{
+    id: '6',
+    name: 'Lady Gaga',
+    imgUrl: 'https://wallpaperaccess.com/full/301267.jpg',
+    genres: ['pop', 'electronic', 'art pop'],
+    origin: {
+      country: 'US',
+      city: 'New york'
+    }
   }
 ];
 
