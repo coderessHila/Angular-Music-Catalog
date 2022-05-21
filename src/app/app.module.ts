16a4d543c4454f68743f46b3c2e5bb6aa712d77b
Hila Sackstein <hila.sackstein@Hilas-MacBook-Pro.local>
2022-05-21 19:48:01 +0300

    added @angular/material - custom theme, yes typography yes animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
 import {FeedModule} from "./feed/feed.module";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import {FeedModule} from "./feed/feed.module";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FeedModule
+    FeedModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/feed/preview-artist/preview-artist.component.ts
+++ src/app/feed/preview-artist/preview-artist.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import {IArtist} from "../../artists-data/artist.interface";
 import {GetArtistsDataService} from "../../get-artists-data.service";
+// import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-preview-artist',
