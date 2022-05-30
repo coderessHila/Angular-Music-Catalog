fea267befaf1e267a91d459c6b3fb0857eedb9ff
HilaFake <hila@fake>
2022-05-30 09:07:45 +0300

    profile btn menu

--- src/app/app-components/header-bar/header-bar.module.ts
+++ src/app/app-components/header-bar/header-bar.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderBarComponent } from './header-bar.component';
 import {RouterModule} from "@angular/router";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {MatMenuModule} from "@angular/material/menu";
 
 
 
@@ -9,10 +12,13 @@ import {RouterModule} from "@angular/router";
   declarations: [
     HeaderBarComponent
   ],
-    imports: [
-        CommonModule,
-        RouterModule
-    ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule
+  ],
   exports: [
     HeaderBarComponent
   ]
