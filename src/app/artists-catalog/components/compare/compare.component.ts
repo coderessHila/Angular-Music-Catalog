fe197f2750ce797bd3dc77e1d3c710ac9a248f11
Hila Sackstein <hila@fake>
2022-06-03 20:03:06 +0300

    sorting works, compare table

--- src/app/artists-catalog/components/compare/compare.component.ts
+++ src/app/artists-catalog/components/compare/compare.component.ts
@@ -16,33 +16,28 @@ export class CompareComponent implements OnInit {
 
 
   constructor(private artistsQuery: ArtistsQuery,
-              private _liveAnnouncer:LiveAnnouncer) {
+              private _liveAnnouncer: LiveAnnouncer) {
     this.artistsQuery.selectAllArtists$.pipe().subscribe(
       artists => {
         this.artists = artists;
-          this.dataSource = new MatTableDataSource<Artist>(artists);
+        this.dataSource = new MatTableDataSource<Artist>(artists);
       }
     )
+    this.dataSource.sortingDataAccessor = this.nestedProperty
   }
 
   @ViewChild(MatSort) sort!: MatSort;
 
+
+  nestedProperty = (data: any, sortHeaderId: string): string | number => {
+    return sortHeaderId
+      .split('.')
+      .reduce((acc, key) => acc && acc[key], data) as string | number;
+  }
+
   ngAfterViewInit() {
     this.dataSource.sort = this.sort;
   }
-
-  /** Announce the change in sort state for assistive technology. */
-announceSortChange(sortState: Sort) {
-  // This example uses English messages. If your application supports
-  // multiple language, you would internationalize these strings.
-  // Furthermore, you can customize the message to add additional
-  // details about the values being sorted.
-  if (sortState.direction) {
-    this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
-  } else {
-    this._liveAnnouncer.announce('Sorting cleared');
-  }
-}
 
   // columns: string[] = ['Name', 'Genres', 'Country', 'City', 'Active Since', 'Popularity']
   columns = [
@@ -85,81 +80,3 @@ announceSortChange(sortState: Sort) {
   }
 
 }
-
-// constructor(private _liveAnnouncer: LiveAnnouncer) {}
-//
-// @ViewChild(MatSort) sort: MatSort;
-//
-// ngAfterViewInit() {
-//   this.dataSource.sort = this.sort;
-// }
-//
-// /** Announce the change in sort state for assistive technology. */
-// announceSortChange(sortState: Sort) {
-//   // This example uses English messages. If your application supports
-//   // multiple language, you would internationalize these strings.
-//   // Furthermore, you can customize the message to add additional
-//   // details about the values being sorted.
-//   if (sortState.direction) {
-//     this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
-//   } else {
-//     this._liveAnnouncer.announce('Sorting cleared');
-//   }
-// }
-
-
-//
-// export interface PeriodicElement {
-//   name: string;
-//   position: number;
-//   weight: number;
-//   symbol: string;
-// }
-//
-// const ELEMENT_DATA: PeriodicElement[] = [
-//   {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-//   {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-//   {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-//   {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-//   {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-//   {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-//   {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-//   {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-//   {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-//   {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-// ];
-//
-// /**
-//  * @title Table with columns defined using ngFor instead of statically written in the template.
-//  */
-// @Component({
-//   selector: 'table-generated-columns-example',
-//   styleUrls: ['table-generated-columns-example.css'],
-//   templateUrl: 'table-generated-columns-example.html',
-// })
-// export class TableGeneratedColumnsExample {
-//   columns = [
-//     {
-//       columnDef: 'position',
-//       header: 'No.',
-//       cell: (element: PeriodicElement) => `${element.position}`,
-//     },
-//     {
-//       columnDef: 'name',
-//       header: 'Name',
-//       cell: (element: PeriodicElement) => `${element.name}`,
-//     },
-//     {
-//       columnDef: 'weight',
-//       header: 'Weight',
-//       cell: (element: PeriodicElement) => `${element.weight}`,
-//     },
-//     {
-//       columnDef: 'symbol',
-//       header: 'Symbol',
-//       cell: (element: PeriodicElement) => `${element.symbol}`,
-//     },
-//   ];
-//   dataSource = ELEMENT_DATA;
-//   displayedColumns = this.columns.map(c => c.columnDef);
-// }
